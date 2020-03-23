9d2e6ef837dedf2f22494d435de22c95afb1e054
rafatmyo <rafatmyo@gmail.com>
2020-03-22 20:00:25 -0600

    Remove: Logic in render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''};
@@ -12,15 +13,23 @@ class App extends React.Component {
         );
     }
 
-    // React says we have to define render
-    render() {
+    renderContent(){
         if (this.state.errorMessage && !this.state.lat){
             return <div> Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat){
             return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div> Loading...</div>;
+        return <Spinner message="Please accept loaction Request"/>;
+    }
+
+    // React says we have to define render
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
